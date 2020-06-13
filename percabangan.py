7b82753493d7bf33dd11a76f63da73adc0bfc3d2
Nanang <prasetiyantara@gmail.com>
2020-06-13 14:50:19 +0700

    materi percabangan

---
+++ percabangan.py
@@ -0,0 +1,12 @@
+
+print('tanggal 13 Juni 2020')
+
+ingin_cepat = True
+if ingin_cepat:
+    print('jalan lurus dan fokus saja')
+else:
+    print('jalan  lain dengan tidak baik')
+
+jml_anak = 4
+for index_anak in range(1, jml_anak+2):
+    print(f'halo anak #{index_anak}')
